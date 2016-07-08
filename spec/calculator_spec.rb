ce3d059a4be05139a53ba4a29b5f7fc532e8b047
Philip Cole Johnson <philipcolejohnson@gmail.com>
2016-07-08 09:26:15 -0700

    Tests for adding and subtracting

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -1,1 +1,28 @@
-# Your code here
+require 'Calculator'
+
+describe Calculator do
+
+  let(:c) { Calculator.new }
+
+  describe "#add" do
+
+    it "returns the sum of two addends" do
+      expect(Calculator.new.add(1, 2)).to eq(3)
+    end
+
+    it "returns the sum with a negative argument" do
+      expect(Calculator.new.add(-3, 3)).to eq(0)
+    end
+  end
+
+  describe "#subtract" do
+    it "returns difference between two arguments" do
+      expect(c.subtract(4,2)).to eq(2)
+    end
+
+    it "returns the correct difference with negative numbers" do
+      expect(c.subtract(-3, -4)).to eq(1)
+    end
+  end
+
+end
